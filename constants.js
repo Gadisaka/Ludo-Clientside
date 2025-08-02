53aa6735107f1530cc180d4fe7e258519dbd3555
gadisa kasaye <gadisaka04@gmail.com>
2025-08-02 12:01:06 +0300

    update on the api url

--- constants.js
+++ constants.js
@@ -1,2 +1,2 @@
-// export const API_URL = "https://ludo-serverside.onrender.com";
-export const API_URL = "http://localhost:4002";
+export const API_URL = "https://ludo-serverside.onrender.com";
+// export const API_URL = "http://localhost:4002";
