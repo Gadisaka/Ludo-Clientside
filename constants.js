8e48820d885d1222d788cb5b17364f7000b9f6f9
gadisa kasaye <gadisaka04@gmail.com>
2025-09-18 14:28:41 +0300

    chore: update API_URL to new production endpoint and comment out previous URL

--- constants.js
+++ constants.js
@@ -1,7 +1,7 @@
 // export const API_URL = "https://ludo-serverside.onrender.com";
-export const API_URL = "https://ethioludo-backend.onrender.com";
+// export const API_URL = "https://ethioludo-backend.onrender.com";
 // export const API_URL = "http://localhost:4002";
-// export const API_URL = "https://api.ludo.ethiostargames.com";
+export const API_URL = "https://api.ludo.ethiobingo.net";
 
 // Game configuration
 export const GAME_CUT_PERCENTAGE = 10; // Default cut percentage for game winnings
